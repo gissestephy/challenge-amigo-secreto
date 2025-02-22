50e77520d1927f8ca22649f2c35856fc795f6b3a
gissestephy <gisela.s.colmeiro@gmail.com>
2025-02-22 12:37:25 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Los usuarios ingresan nombres de amigos en una lista para luego sortearlo de manera aleatoria y determinar quien es el amigo secreto
+// El usuario agrega nombres en un campo de texto y un boton Adicionar
+// Los nombres ingresados se deben mostrar en una lista visible en la misma pagina
+// Funcional: boton Adicionar para agregar los amigos en una misma pagina
+// Boton Sortear amigo: seleccionar uno de los nombres y mostrara el resultado en pantalla
+
+// FUNCIONALIDADES
+// - Agregar nombres: usuario escribe el nombre y se agrega a una lista visible cuando toca Addicionar
+// - Validar entrada: si el campo de texto esta vacio, el programa mostrara una alertga pidiendo un nombre valido
+// - Visualizar la lista: los nombres ingresados apareceran en una lista debajo del campo de entrada
+// - Sorteo Aleatorio: al hacer clic sobre el boton se selecciona un nombre y se muestra en la pagina
+
+// Crear un array para almacenar los nombres
+let amigos = []
